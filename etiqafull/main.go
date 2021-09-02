e994c9aa85f507c3e89a660adfa7bdff3949c8b3
Sundip-dev <sundip.s@aviabird.com>
2021-09-02 15:40:53 +0530

    Etiqa Changes

--- etiqafull/main.go
+++ etiqafull/main.go
@@ -1,10 +1,8 @@
 package main
 
-import "example.com/user/hello/etiqa/etiqafull/etutil"
-
 func main() {
 	//etutil.GetVehicleList()
 	//etutil.VehilceDetails()
 	//etutil.GetPremium()
-	etutil.InitiateTransaction()
+	//etutil.InitiateTransaction()
 }
